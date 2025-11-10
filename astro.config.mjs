5541152c463fe62e134c91f26f23cbe26b9801c5
nikiandr <inbox@nfordui.com>
2025-11-10 15:44:14 +0200

    add agents file + updated main sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    integrations: [tailwind()],
+    redirects: {
+      "/": { status: 302, destination: "/about-me" },
+    },
+    prefetch: true,
 });
