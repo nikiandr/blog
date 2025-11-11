f3f666ac9dca7420d718c546a30abb2c4589546e
nikiandr <inbox@nfordui.com>
2025-11-11 15:17:14 +0200

    rendered albums list

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./articles" }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/articles" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -13,4 +13,26 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const albums = defineCollection({
+  loader: glob({
+    pattern: "**/*.json",
+    base: "./src/albums",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      draft: z.boolean().optional(),
+      tripDates: z.array(z.coerce.date()).length(2),
+      photosFolder: z.string(),
+      photos: z.array(
+        z.object({
+          photo: image(),
+          description: z.string(),
+        })
+      ),
+    }),
+});
+
+export const collections = { blog, albums };
