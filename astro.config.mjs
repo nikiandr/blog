a864de4c187376fcec496b338c8d7542aceec7c9
nikiandr <inbox@nfordui.com>
2025-11-11 11:17:48 +0200

    topbar updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 
 import mdx from "@astrojs/mdx";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [tailwind(), mdx()],
   prefetch: true,
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
